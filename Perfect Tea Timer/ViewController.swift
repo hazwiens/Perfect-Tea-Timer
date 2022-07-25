5e9b76ec0317c5531c4298a5e0c27bf9d7fa4067
hazwiens <haz.wiens@icloud.com>
2022-07-25 06:00:34 -0600

    First commit

--- Perfect Tea Timer/ViewController.swift
+++ Perfect Tea Timer/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var doneLabel: UILabel!    
+    @IBOutlet weak var choiceLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func timeSelected(_ sender: UIButton) {
+        
+    }
+    
 }
 
